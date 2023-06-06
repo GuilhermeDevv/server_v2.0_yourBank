34f647df7e02608611040d36143ffe797cfb7e7e
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-06-06 17:57:40 -0300

    adjustment at update user interface

--- src/modules/user/repositories/UserRepository.ts
+++ src/modules/user/repositories/UserRepository.ts
@@ -1,10 +1,14 @@
 import { CreateUserDTO } from '../dtos/createUserDTO';
+import { UpdateUserDTO } from '../dtos/updateUserDTO';
 import { UserDTO } from '../dtos/userDTO';
 
 export abstract class UserRepository {
   abstract findByEmail(email: string): Promise<UserDTO | null>;
   abstract create({ email, name, password }: CreateUserDTO): Promise<boolean>;
-  abstract update();
+  abstract update(
+    data: Partial<UpdateUserDTO>,
+    email: string,
+  ): Promise<boolean>;
   abstract transaction();
   abstract transactionLog();
 }
